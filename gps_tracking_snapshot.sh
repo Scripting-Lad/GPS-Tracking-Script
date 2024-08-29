e88b8a6798db02a009f7600025c978beec93c338
Scripting-Lad <100461985+Scripting-Lad@users.noreply.github.com>
2024-08-29 17:57:53 -0600

    Update v1.1 - gps_tracking_snapshot.sh
    Changes:
    - Added Read-Only Associative Array SV_Array
    - Removed repetitive lines
    - Fixed bug where Altitude would not report 
    - (FIX) Replaced portion of line 11 with <grep -m 2 TPV>

--- gps_tracking_snapshot.sh
+++ gps_tracking_snapshot.sh
@@ -8,205 +8,52 @@ coor=$(gpspipe -w -n 10 | grep -m 1 TPV | jq -r '[.lat, .lon] | @csv')
 echo Coordinates: $coor
 echo " "
 
-alt=$(gpspipe -w -n 10 | grep -m 1 TPV | jq -r '[.altHAE] | @csv')
+alt=$(gpspipe -w -n 10 | grep -m 2 TPV | jq -r '[.altHAE] | @csv')
 echo Altitude\(M\): $alt
 echo " "
 
-echo Current IIR GPS-Lockheed Satellites in View:
-for sv in $(gpspipe -w -n 12 |  grep -oP '\{"PRN":.*?\}' |  grep -oP '\{"PRN":\b(13|28|21|11|20|18|16|19|2)\b.*?\}'| sort -u -t, -k1,1 | awk -F, '{print $1}' | sed 's/{"PRN"://' )
+
+declare -rA SV_Array=(
+["13"]="SV43-IIR-Lockheed"
+["22"]="SV44-IIR-Lockheed"
+["21"]="SV45-IIR-Lockheed"
+["11"]="SV46-IIR-Lockheed"
+["20"]="SV51-IIR-Lockheed"
+["18"]="SV54-IIR-Lockheed"
+["16"]="SV56-IIR-Lockheed"
+["19"]="SV59-IIR-Lockheed"
+["2"]="SV61-IIR-Lockheed"
+["7"]="SV48-IIRM-Lockheed"
+["5"]="SV50-IIRM-Lockheed"
+["31"]="SV52-IIRM-Lockheed"
+["17"]="SV53-IIRM-Lockheed"
+["15"]="SV55-IIRM-Lockheed"
+["29"]="SV57-IIRM-Lockheed"
+["12"]="SV58-IIRM-Lockheed"
+["1"]="SV62-Boeing"
+["3"]="SV63-Boeing"
+["6"]="SV64-Boeing"
+["8"]="SV65-Boeing"
+["9"]="SV66-Boeing"
+["10"]="SV67-Boeing"
+["24"]="SV68-Boeing"
+["25"]="SV69-Boeing"
+["26"]="SV70-Boeing"
+["27"]="SV71-Boeing"
+["30"]="SV72-Boeing"
+["32"]="SV73-Boeing"
+["4"]="SV74-III-Lockheed"
+["18"]="SV75-III-Lockheed"
+["23"]="SV76-III-Lockheed"
+["14"]="SV77-III-Lockheed"
+["11"]="SV78-III-Lockheed"
+["28"]="SV79-III-Lockheed"
+)
+
+
+echo Current GPS Satellites in View:
+for sv in $(gpspipe -w -n 12 |  grep -oP '\{"PRN":.*?\}' | grep -oP '\{"PRN":\b(13|22|21|11|20|18|16|19|2|1|5|31|17|15|29|12|1|3|6|8|9|10|24|25|26|27|30|32|4|18|23|14|11|28)\b.*?\}' | sort -u -t, -k1,1 | awk -F, '{print $1}' | sed 's/{"PRN"://' )
 do
-	if [ $sv -eq 13 ]
-	then
-		echo SV43
-	fi
-
-	if [ $sv == 28 ]
-	then
-		echo SV44
-	fi
-
-	if [ $sv == 21 ]
-	then
-		echo SV45
-	fi
-
-	if [ $sv == 11 ]
-	then
-		echo SV46
-	fi
-
-	if [ $sv == 20 ]
-	then
-		echo SV51
-	fi
-
-	if [ $sv == 18 ]
-	then
-		echo SV54
-	fi
-
-	if [ $sv == 16 ]
-	then
-		echo SV56
-	fi
-
-	if [ $sv == 19 ]
-	then
-		echo SV59
-	fi
-
-	if [ $sv == 2 ]
-	then
-		echo SV61
-	fi
+	echo ${SV_Array[$sv]}
 done
-echo " "
-
-
-echo Current IIRM GPS-Lockheed Satellites in View:
-for sv in $(gpspipe -w -n 12 |  grep -oP '\{"PRN":.*?\}' |  grep -oP '\{"PRN":\b(7|1|5|31|17|15|29|12)\b.*?\}'| sort -u -t, -k1,1 | awk -F, '{print $1}' | sed 's/{"PRN"://' )
-do
-	if [ $sv -eq 7 ]
-	then
-		echo SV48
-	fi
-
-	if [ $sv == 1 ]
-	then
-		echo SV49
-	fi
-
-	if [ $sv == 5 ]
-	then
-		echo SV50
-	fi
-
-	if [ $sv == 31 ]
-	then
-		echo SV52
-	fi
-
-	if [ $sv == 17 ]
-	then
-		echo SV53
-	fi
-
-	if [ $sv == 15 ]
-	then
-		echo SV55
-	fi
-
-	if [ $sv == 29 ]
-	then
-		echo SV57
-	fi
-
-	if [ $sv == 12 ]
-	then
-		echo SV58
-	fi
-done
-echo " "
-
-
-echo Current IIF GPS-Boeing Satellites in View:
-for sv in $(gpspipe -w -n 12 |  grep -oP '\{"PRN":.*?\}' |  grep -oP '\{"PRN":\b(1|3|6|8|9|10|24|25|26|27|30|32)\b.*?\}'| sort -u -t, -k1,1 | awk -F, '{print $1}' | sed 's/{"PRN"://' )
-do
-	if [ $sv -eq 25 ]
-	then
-		echo SV62
-	fi
-
-	if [ $sv == 1 ]
-	then
-		echo SV63
-	fi
-
-	if [ $sv == 30 ]
-	then
-		echo SV64
-	fi
-
-	if [ $sv == 24 ]
-	then
-		echo SV65
-	fi
-
-	if [ $sv == 27 ]
-	then
-		echo SV66
-	fi
-
-	if [ $sv == 6 ]
-	then
-		echo SV67
-	fi
-
-	if [ $sv == 9 ]
-	then
-		echo SV68
-	fi
-
-	if [ $sv == 3 ]
-	then
-		echo SV69
-	fi
-
-	if [ $sv == 32 ]
-	then
-		echo SV70
-	fi
-
-	if [ $sv == 26 ]
-	then
-		echo SV71
-	fi
-
-	if [ $sv == 8 ]
-	then
-		echo SV72
-	fi
-
-	if [ $sv == 10 ]
-	then
-		echo SV73
-	fi
-done
-echo " "
-
-
-echo Current III GPS-Lockheed Satellites in View:
-for sv in $(gpspipe -w -n 12 |  grep -oP '\{"PRN":.*?\}' |  grep -oP '\{"PRN":\b(4|18|23|14|11|28)\b.*?\}'| sort -u -t, -k1,1 | awk -F, '{print $1}' | sed 's/{"PRN"://' )
-do
-	if [ $sv -eq 4 ]
-	then
-		echo SV74
-	fi
-
-	if [ $sv == 18 ]
-	then
-		echo SV75
-	fi
-
-	if [ $sv == 23 ]
-	then
-		echo SV76
-	fi
-
-	if [ $sv == 14 ]
-	then
-		echo SV77
-	fi
-
-	if [ $sv == 11 ]
-	then
-		echo SV78
-	fi
-
-	if [ $sv == 28 ]
-	then
-		echo SV79
-	fi
-done
-
 echo " "
